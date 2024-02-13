9b5e0bbd060e69ab06f2ffa544d73b5c0409f79e
Gleb Rasskazov <glebunium146@gmail.com>
2024-02-13 18:22:59 +0300

    initial

---
+++ Slide Out Menu/Slide Out Menu/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Slide Out Menu
+//
+//  Created by Gleb Rasskazov on 13.02.2024.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
+
---
+++ Slide Out Menu/Slide Out Menu/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Slide Out Menu
+//
+//  Created by Gleb Rasskazov on 13.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemIndigo
+    }
+}
+
